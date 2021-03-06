e033e445729f9d02a75ad3d7367531ada5ef9988
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2021-03-05 19:51:06 -0500

    fixed db setup

--- test/usersTest.js
+++ test/usersTest.js
@@ -6,21 +6,14 @@ const db = new Pool(dbParams);
 db.connect();
 
 describe('Login user', function () {
-  it('should return a user if user exists in db', function () {
+  it('should return a user if user exists in db', function() {
     let username = 'JohnSmith';
     let password = "password";
     let expectedOutput = 1;
-    db.query(`
+    let dbquery = db.query(`
       SELECT * FROM users
       WHERE username = $1 AND password = $2;
     `, [username, password]);
-    assert.equal(db.query(`
-    SELECT * FROM users
-    WHERE username = $1 AND password = $2;
-  `, [username, password]), expectedOutput);
-  });
-  it('if email does not exist it should return undefined', function () {
-    const user = getUserByEmail("hellothere@example.com", testUsers);
-    assert.isUndefined(user);
+    assert.equal(dbquery, expectedOutput);
   });
 });
